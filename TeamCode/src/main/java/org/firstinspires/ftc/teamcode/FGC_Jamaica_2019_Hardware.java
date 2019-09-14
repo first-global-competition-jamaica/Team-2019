f5e67e5adefa1f7a8bbc8ef5bde9cb846e0e534e
kkbrown123 <brownkevonteh@gmail.com>
2019-09-14 14:49:20 -0700

    i hardward mapped the lift motors and the limit switch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FGC_Jamaica_2019_Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FGC_Jamaica_2019_Hardware.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -17,6 +18,9 @@ public class FGC_Jamaica_2019_Hardware {
     public DcMotor BRight    = null;
     public DcMotor BLeft     = null;
     public DcMotor Intake    = null;
+    public DcMotor lift_motor1 = null;
+    public DcMotor lift_motor2 = null;
+    public DigitalChannel limit_lift_switch = null;
     public HardwareMap hwmap;
 
 
@@ -28,12 +32,17 @@ public class FGC_Jamaica_2019_Hardware {
      BRight = hwmap.get(DcMotor.class, "BRight");
      BLeft  = hwmap.get(DcMotor.class, "BLeft");
      Intake = hwmap.get(DcMotor.class, "Intake");
-
+     limit_lift_switch = hwmap.get(DigitalChannel.class, "lift_switch");
+     lift_motor1 = hwmap.get(DcMotor.class, "limit_Left");
+     lift_motor2 = hwmap.get(DcMotor.class, "limit_right");
 
      FLeft.setDirection(DcMotor.Direction.FORWARD);
      FRight.setDirection(DcMotor.Direction.REVERSE);
      BLeft.setDirection(DcMotor.Direction.FORWARD);
      BRight.setDirection(DcMotor.Direction.REVERSE);
+     lift_motor1.setDirection(DcMotor.Direction.REVERSE);
+     lift_motor2.setDirection(DcMotor.Direction.REVERSE);
+     limit_lift_switch.setMode(DigitalChannel.Mode.INPUT);
 
  }
 }
